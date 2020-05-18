b23190733670897699ae599f99a8d319bf8efd2a
Nikita PC <lastlasta@Gmail.com>
2020-05-18 12:41:19 +0300

    done

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <ctime>
 #include <vector>
 
@@ -7,28 +7,30 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-
 vector<vector<float>> matrix;
+// Абстрактный класс
 class Matrix
 {
 public:
+    // чисто виртуальная функиция
     virtual void show_matrix() = 0;
-
+    // Деструктор
     ~Matrix(){}
 };
-
+// Класс "исходная матрица"
 class SourceMatrix : public Matrix
 {
 private:
-    int columns, lines;
+    int columc_end, lines;
     
 public:
+    // Конструктор по умолчанию
     SourceMatrix()
 	{
-		columns = 2;
+		columc_end = 2;
 		lines = 2;
 	}
-
+    // Метод
 	void made_matrix()
 	{
         cout << "Enter the number of lines <16" << endl;
@@ -45,11 +47,11 @@ public:
             }
         }
 
-        cout << "Enter the number of columns <21" << endl;
+        cout << "Enter the number of columc_end <21" << endl;
         while (1)
         {
-            cin >> columns;
-            if (columns < 21 && columns > 0)
+            cin >> columc_end;
+            if (columc_end < 21 && columc_end > 0)
             {
                 break;
             }
@@ -64,35 +66,35 @@ public:
         for (int i = 0; i < lines; i++)
         {
             matrix.push_back(vector<float>()); 
-            for (int j = 0; j < columns; j++)
+            for (int j = 0; j < columc_end; ++j)
             {
                 matrix[i].push_back(rand() % 10); 
             }
         }
 	}
-
+    // Метод (оверрайд для виртуальной функции) 
     void show_matrix() override
     {
         cout << "----Source matrix----";
-        for (int i = 0; i < lines; i++)
+        for (int i = 0; i < lines; ++i)
         {
             cout << endl;
-            for (int j = 0; j < columns; j++)
+            for (int j = 0; j < columc_end; ++j)
             {
-                cout << matrix[i][j] << "\t";
+                cout << matrix[i][j] << "  ";
             }
         }
     }
-
+    // Деструктор
     ~SourceMatrix() {}
 };
 
-
-class TransMatrix : public Matrix
+// Класс изм. матрицы
+class TransMatrix : public SourceMatrix
 {
 private:
     int l_start, l_end, c_start, c_end;
-
+    
 public: 
     TransMatrix() 
     {
@@ -135,7 +137,7 @@ public:
 
         while (1)
         {
-            cout << endl << "Enter the start of the columns spacing..." << endl;
+            cout << endl << "Enter the start of the columc_end spacing..." << endl;
             cin >> c_start;
             if (c_start > 0)
             {
@@ -161,23 +163,69 @@ public:
             }
         }
     }
-
+    // Метод (оверрайд для вирутальной фукнции)
     void show_matrix() override
     {
         cout << endl << "---Transformed matrix---";
-        for (int i = l_start - 1; i < l_end; i++)
+        for (int i = l_start - 1; i < l_end; ++i)
         {
             cout << endl;
-            for (int j = c_start - 1; j < c_end; j++)
+            for (int j = c_start - 1; j < c_end; ++j)
             {
-                cout << matrix[i][j] << "\t";
+                cout << matrix[i][j] << "  ";
             }
         }
     } 
+    
+    void mathexp()
+    {
+        int average, sh = 0, sum = 0;
+        for (int i = l_start - 1; i < l_end; ++i)
+        {
+            for (int j = c_start - 1; j < c_end; ++j)
+            {
+                sh++;
+                sum += matrix[i][j];
+            }
+        }
+        average = sum / sh;
+        cout << endl << endl << "Mathexp: " << average;
+    }
+    
+    void finding_mode() 
+    {
+        vector<float> vec;
+        int k = -1;
+        float maxValue = 0, maxCount = -1;
+        for (int j = l_start - 1; j <= l_end - 1; ++j)
+        {
+            for (int i = c_start - 1; i <= c_end - 1; ++i)
+            {
+                k++;
+                vec.push_back(matrix[i][j]);
+            }
+        }
 
+        for (int i = 0; i < k; ++i)
+        {
+            int count = 0;
+            for (int j = 0; j < k; ++j)
+            {
+                if (vec[j] == vec[i])
+                    ++count;
+            }
+            if (count > maxCount)
+            {
+                maxCount = count;
+                maxValue = vec[i];
+            }
+        }
+        cout <<  endl <<  "Mode: " << maxValue;
+    }
+    // Деструктор
     ~TransMatrix() {}
 };
-
+// класс для вывода матриц (полиморфизм)
 class Display
 {
 public:
@@ -193,11 +241,13 @@ int main()
     SourceMatrix source_matrix;
     source_matrix.made_matrix();
     print.show_matrix(&source_matrix);
-    TransMatrix trans_matrix;
-    trans_matrix.line_spacing();
+    TransMatrix trabs_matrix;
+    trabs_matrix.line_spacing();
     system("cls");
     print.show_matrix(&source_matrix);
-    print.show_matrix(&trans_matrix);
-  
+    print.show_matrix(&trabs_matrix);
+    trabs_matrix.mathexp();
+    trabs_matrix.finding_mode();
+
 	return 0;
 }
